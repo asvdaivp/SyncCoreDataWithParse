1ff5e5f385530f76609288c438b3ea290ec28e49
daivp <daivp@aoi-sys.vn>
2015-09-29 17:10:57 +0700

    init project

---
+++ SyncCoreDataToParse/SyncCoreDataToParse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SyncCoreDataToParse
+//
+//  Created by phuocdai on 9/29/15.
+//  Copyright © 2015 phuocdai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
